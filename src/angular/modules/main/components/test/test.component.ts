d406ab61ce36a58d6d10306e43b1088f561a61dc
Vincent Callede <vincent.callededev@gmail.com>
2025-10-23 15:12:03 +0200

    added test component

---
+++ src/angular/modules/main/components/test/test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'main-root',
+  imports: [RouterOutlet],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.scss'
+})
+export class MainRootComponent {
+
+}
